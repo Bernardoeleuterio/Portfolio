7b4873a8d739979d90c82717b3b23123f6088323
Bernardo <bernardomoraes1727@gmail.com>
2026-05-20 19:32:22 -0300

    feat: Criação do Sobre

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
 import Contato from './components/Contato'
+import Sobre from './components/Sobre'
 
  export default function App() {
    return(
     <>
     <Navbar />
     <Home />
+    <Sobre />
     <Contato />
     </>
     
---
+++ src/components/Sobre.jsx
@@ -0,0 +1,60 @@
+import "../styles/sobre.css";
+
+import {
+  FaGraduationCap,
+  FaBriefcase,
+  FaReact,
+  FaPython,
+  FaPhp,
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaNodeJs,
+  FaGitAlt
+} from "react-icons/fa";
+
+export default function Sobre() {
+  return (
+    <section className="sobre" id="sobre">
+      <div className="sobre-container">
+        <h2>Sobre Mim</h2>
+
+        <p className="descricao-sobre">
+          Sou estudante de Engenharia de Software e apaixonado por tecnologia.
+          Atualmente desenvolvo projetos web e acadêmicos buscando minha primeira oportunidade na área.
+        </p>
+
+        <div className="cards-sobre">
+          <div className="card-sobre">
+            <FaGraduationCap className="card-icon" />
+            <h3>Formação</h3>
+            <p>Engenharia de Software</p>
+          </div>
+
+          <div className="card-sobre tecnologias-card">
+            <h3>Tecnologias</h3>
+
+            <div className="tecnologias-icons">
+              <FaHtml5 className="html" />
+              <FaCss3Alt className="css" />
+              <FaJs className="js" />
+              <FaReact className="react" />
+              <FaPython className="python" />
+              <FaPhp className="php" />
+              <FaNodeJs className="nodejs" />
+              <FaGitAlt className="git" />
+            </div>
+          </div>
+
+          <div className="card-sobre">
+            <FaBriefcase className="card-icon" />
+            <h3>Objetivo</h3>
+            <p>Primeira oportunidade na área</p>
+          </div>
+
+         
+        </div>
+      </div>
+    </section>
+  );
+}
